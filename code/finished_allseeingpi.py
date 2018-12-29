41bc817522df6d304e572f4bf48440e4028253d0
Sérgio Salgueiro <45968496+SergioCwb@users.noreply.github.com>
2018-12-29 12:11:00 -0200

    Enable external button operation
    If you won't use a touch screen, you can enable the external buttons to take a new picture.
    When the preview screen is active, the left button changes the overlays, and the right one takes the picture.
    Once the picture is taken, any button can be used to restart the preview.

--- code/finished_allseeingpi.py
+++ code/finished_allseeingpi.py
@@ -5,43 +5,68 @@ from time import gmtime, strftime
 from guizero import App, PushButton, Text, Picture
 from twython import Twython
 import time
+import sys
+"""
 from auth import (
     consumer_key,
     consumer_secret,
     access_token,
     access_token_secret
 )
+"""
+
+def btn_left():
+    global preview_active
+    if (preview_active):
+        next_overlay()
+    else:
+        new_picture()
+    
+def btn_right():
+    global preview_active
+    if (preview_active):
+        take_picture()
+    else:
+        new_picture()
+        preview_active = True    
 
 # Tell the next overlay button what to do
 def next_overlay():
     global overlay
+    global preview_active
     overlay = next(all_overlays)
     preview_overlay(camera, overlay)
 
 # Tell the take picture button what to do
 def take_picture():
     global output
-    output = strftime("/home/pi/allseeingpi/image-%d-%m %H:%M.png", gmtime())
-    time.sleep(3) # wait 3 seconds to avoid blurry pictures when pressing the button
+    global preview_active
+    output = strftime("/home/pi/allseeingpi/image-%d-%m_%H:%M:%s.png", gmtime())
+    time.sleep(3)
     camera.capture(output)
     camera.stop_preview()
     remove_overlays(camera)
     output_overlay(output, overlay)
 
     # Save a smaller gif
-    size = 400, 400
+    size = 800, 600
     gif_img = Image.open(output)
     gif_img.thumbnail(size, Image.ANTIALIAS)
     gif_img.save(latest_photo, 'gif')
 
     # Set the gui picture to this picture
     your_pic.set(latest_photo)
+    
+    # Reset the buttons
+    preview_active = False
 
 
 def new_picture():
-    camera.start_preview(alpha=128)
+    global preview_active
+    remove_overlays(camera)
+    camera.start_preview()
     preview_overlay(camera, overlay)
-
+    preview_active = True
 
 def send_tweet():
     twitter = Twython(
@@ -56,29 +81,36 @@ def send_tweet():
     with open(output, 'rb') as photo:
         twitter.update_status_with_media(status=message, media=photo)
 
+def getOut():
+    sys.exit()
+
 # Set up buttons
 next_overlay_btn = Button(23)
-next_overlay_btn.when_pressed = next_overlay
+next_overlay_btn.when_pressed = btn_left #next_overlay
 take_pic_btn = Button(25)
-take_pic_btn.when_pressed = take_picture
+take_pic_btn.when_pressed = btn_right #take_picture
 
 # Set up camera (with resolution of the touchscreen)
 camera = PiCamera()
-camera.resolution = (1024, 768)
+camera.resolution = (800, 600) # 800, 480
 camera.hflip = True
 
 # Start camera preview
-camera.start_preview(alpha=128)
+camera.start_preview()
+
+# Enable external buttons functions
+preview_active = True
 
 # Set up filename
 output = ""
 
 latest_photo = '/home/pi/allseeingpi/latest.gif'
 
-app = App("The All Seeing Pi", 800, 480)
-#app.tk.attributes("-fullscreen", True)
-message = Text(app, "I spotted you!")
+app = App("@CabineDeFotos on Twitter!", 1024, 800)
+# app.tk.attributes("-fullscreen", True)
+message = Text(app, "The All Seeing Pi saw you!")
 your_pic = Picture(app, latest_photo)
-new_pic = PushButton(app, new_picture, text="New picture")
-tweet_pic = PushButton(app, send_tweet, text="Tweet picture")
+new_pic = PushButton(app, new_picture, text="New Picture")
+# tweet_pic = PushButton(app, send_tweet, text="Tweet picture")
+getOut = PushButton(app, getOut, text="Quit")
 app.display()
